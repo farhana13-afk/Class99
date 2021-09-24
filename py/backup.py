43743b4249a87db2626d06c85b9682e8613e97f7
farhana13-afk <73855710+farhana13-afk@users.noreply.github.com>
2021-09-24 16:58:06 -0400

    Add files via upload

---
+++ py/backup.py
@@ -0,0 +1,8 @@
+import os 
+import shutil
+
+sourcePath = "C:\\Users\\mashi\\Desktop\\farhana\\python\\Class99\\file1.txt"
+
+destinationPath = "C:\\Users\\mashi\\Desktop\\farhana\\python\\Class99\\file3.txt"
+
+test = shutil.move(sourcePath, destinationPath)
